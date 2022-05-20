fa289455e2b19f22b9186df3864e8536d3ac4ad7
fire <fire@b3dmultitech.com>
2022-05-20 17:31:46 -0600

    add CRUD interface for users

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class UserRepository
+ * @package App\Repositories
+ * @version May 20, 2022, 11:28 pm UTC
+*/
+
+class UserRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return User::class;
+    }
+}
---
+++ resources/views/users/table.blade.php
@@ -0,0 +1,34 @@
+<div class="table-responsive-sm">
+    <table class="table table-striped" id="users-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+        <th>Email</th>
+        <th>Email Verified At</th>
+        <th>Password</th>
+        <th>Remember Token</th>
+                <th colspan="3">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->email_verified_at }}</td>
+            <td>{{ $user->password }}</td>
+            <td>{{ $user->remember_token }}</td>
+                <td>
+                    {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
+                    <div class='btn-group'>
+                        <a href="{{ route('users.show', [$user->id]) }}" class='btn btn-ghost-success'><i class="fa fa-eye"></i></a>
+                        <a href="{{ route('users.edit', [$user->id]) }}" class='btn btn-ghost-info'><i class="fa fa-edit"></i></a>
+                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
