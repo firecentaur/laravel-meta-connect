c34e525114e1e1cb5f95bd593cc2533233108dab
fire <fire@b3dmultitech.com>
2022-05-21 13:29:35 -0600

    updated routes to make main login screen display when visiting website

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['prefix' => 'v1'], function () {
+    return
+        [ Route::post('/user/register', [\App\Http\Controllers\UserController::class, 'registerSlUser']),
+          Route::post('/register', 'Auth\RegisterController@register')
+        ];
+
+});
 
 
 Route::resource('quizzes', App\Http\Controllers\API\QuizAPIController::class);
